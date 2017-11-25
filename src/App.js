7bfd8670d238351d8fa3c01338a8520099f91976
d3ming <d3ming@gmail.com>
2017-11-24 20:48:01 -0800

    Firebase WIP

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import React from 'react';
+import React from "react";
 
 import "./App.css";
 
 import { MingdayAppContainer } from "./containers/MingdayAppContainer";
 
 // Redux
-import { Provider } from 'react-redux';
-import { configureStore } from './redux/configureStore';
+import { Provider } from "react-redux";
+import { configureStore } from "./redux/configureStore";
 
 const store = configureStore();
 
---
+++ src/firebase/config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyCfjgnjFnaBx1qwDXe9Y3YVEN60AcceadY",
+  authDomain: "dm-mingday.firebaseapp.com",
+  databaseURL: "https://dm-mingday.firebaseio.com",
+  storageBucket: "dm-mingday.appspot.com",
+  messagingSenderId: "47122898764",
+  projectId: "dm-mingday"
+};
